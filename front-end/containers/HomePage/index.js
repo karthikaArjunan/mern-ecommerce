4545b3144e3c886dba53787a24272fcf79e2546a
karthikaArjunan <71118073+karthikaArjunan@users.noreply.github.com>
2022-05-28 16:01:18 +0530

    create-frontend

---
+++ front-end/containers/HomePage/index.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Layout } from '../../components/Layout'
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import './style.css'
+/**
+* @author
+* @function HomePage
+**/
+
+export const HomePage = (props) => {
+  return(
+    <Layout>
+      <div>
+          <div class="cartt" ><i>Oremon</i></div>
+          <div>
+<div class="carttt">Organic farming is a type of agriculture or farming which avoids the use of synthetic fertilizers, pesticides, growth regulators, and livestock feed additives. Organic farming systems rely on crop rotation, crop residues, animal manures, legumes, green manure, off-farm organic wastes and bio-fertilizers, mechanical cultivation, mineral bearing rocks. All kinds of agricultural products can be produced organically, including grains, meat, dairy, eggs, fibres such as cotton, jute, flowers etc. Thus organic farming creates a sustainable lifestyle for generations to come.
+</div>
+Ecological benefits of Organic farming
+Organic farmers build healthy soils by nourishing the living component of the soil, the microbial inhabitants that release, transform, and transfer nutrients. Soil organic matter contributes to good soil structure and water-holding capacity. 
+Organic farmers feed soil biota and build soil organic matter with cover crops, compost, and biologically based soil amendments. These produce healthy plants that are better able to resist disease and insect predation. Organic farmers’ primary strategy in controlling pests and diseases is prevention through good plant nutrition and management. 
+Organic farmers use cover crops and sophisticated crop rotations to change the field ecology, effectively disrupting habitat for weeds, insects, and disease organisms.
+ Weeds are controlled through crop rotation, mechanical tillage, and hand-weeding, as well as through cover crops, mulches, flame weeding, and other management methods. 
+Organic farms can support substantially higher levels of wildlife especially in low lands and where animals can roam in pastures or graze on grassland. Not only does wildlife benefit, but entire ecosystems and ground water are improved by simply following organic farming methods.
+Organic farming is much better for the environment as the energy consumption is much less than in the chemical farming.
+It also uses less manurial inputs and completely avoids the synthetic fertilizers which otherwise pollute the soil, water and air.
+Organic farming releases much less carbon dioxide than other farming system.
+Economical benefits of Organic farming
+By switching to organic farming, conventional farmer can actually reduce its production cost by over 25% as compared to the cost of conventional farming. This will eliminate the use of expensive synthetic fertilizers and pesticides, minimizing soil erosion by up to 50% and increasing crop yields up to five-folds.
+Organic farming practices not only benefit farmers and consumers; but the dairies can benefit. When dairies feed their cows organic feed and graze them on organic fields, the cows experience better health, less sickness, diseases and ultimately produce better tasting milk for consumers.
+Reduction in the use of external inputs and increase in output of organic produces with greater potential to benefit the health of farmers and consumers. 
+More Productivity through the incorporation of natural process like natural cycles, nitrogen fixation and pest-predator relationship into the agricultural production process. 
+Greater productive use of the biological and genetic potential of plant and animal species. Long term sustainability of production levels. Profitable and efficient production with emphasis on improved management and conservation of soil, water, energy and biological resources.
+Social Benefits of Organic farming
+It can be adopted by small farms and benefit marginal farmers.
+It could reduce dependency on external inputs and costly technologies thus reducing the competitiveness and disparity among the farmers in a community. 
+It will also lead to food security at the family level and national level. 
+Organic farming is revival of a culture and brings back the indigenous knowledge, beliefs and value system that are almost on extinction now.
+It also contributes to employment generation at the community level.</div>
+      </div>
+    </Layout>
+   )
+ }
